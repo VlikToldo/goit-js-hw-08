7f671453c0712a9758ba77e87d1f16b505916da9
VlikToldo <108451651+VlikToldo@users.noreply.github.com>
2022-11-19 04:07:08 +0300

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
+
+const listEl = document.querySelector('.gallery');
+
+const imgEl = galleryItems
+  .map(({ preview, original, description }) => 
+   `<a class="gallery__item" href="${original}">
+       <img class="gallery__image" 
+       src="${preview}" 
+       alt="${description}"
+       title="${description}" />
+    </a>`)
+  .join("");
+
+  listEl.insertAdjacentHTML("beforeend", imgEl);
+
+  new SimpleLightbox('.gallery a', { 
+    captionData: 'title',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+    overlayOpacity: 0.8,
+    scrollZoom: false,
+   });
 
 console.log(galleryItems);
