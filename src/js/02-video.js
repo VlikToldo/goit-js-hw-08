0ec6146864e77ebcbfa539876420758c18cdca9f
VlikToldo <108451651+VlikToldo@users.noreply.github.com>
2022-11-23 22:26:45 +0300

    1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,11 @@
+// const iframe = document.querySelector('iframe');
+// const player = new Vimeo.Player(iframe);
 
+
+// const onPlay = function(data) {
+//     console.log('ghbdtn')
+// };
+
+// player.on('play', onPlay);
+
+console.log('привет55555');
