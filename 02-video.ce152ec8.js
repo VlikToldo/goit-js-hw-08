e9097dc0c9afa0fe5ea646c7b829be0f24249caa
VlikToldo <108451651+VlikToldo@users.noreply.github.com>
2022-11-29 15:25:03 +0000

    Deploying to gh-pages from @ VlikToldo/goit-js-hw-08@6fb6e8df379f499a7cd794eb3de6067ed5a3097b 🚀

--- 02-video.ce152ec8.js
+++
@@ -1,2 +0,0 @@
-!function(){var o=document.querySelector("a");console.log(o)}();
-//# sourceMappingURL=02-video.ce152ec8.js.map
--- 02-video.f532812e.js
+++
@@ -1,2 +0,0 @@
-const o=document.querySelector("a");console.log(o);
-//# sourceMappingURL=02-video.f532812e.js.map
